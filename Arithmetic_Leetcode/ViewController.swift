18caa5e2fef3968f4d02852028368dd4143a39c4
hanserHe <94189786@qq.com>
2018-09-10 09:59:09 +0800

    first commit

---
+++ Arithmetic_Leetcode/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Arithmetic_Leetcode
+//
+//  Created by Hanser on 2018/9/10.
+//  Copyright © 2018 Mr.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "ArithemeticList"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initComponent() {
+        
+    }
+
+    
+    
+}
+
